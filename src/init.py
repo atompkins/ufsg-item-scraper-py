eebf447eca756084a6622b6072409b8135379023
atompkins <atompkins@gmail.com>
2021-11-03 19:59:42 +0000

    full run

--- src/init.py
+++ src/init.py
@@ -7,14 +7,14 @@ begin = 'index.php?cmd=items&index=0'
 
 async def get_items(session, url):
   anchor = await process_index_page(session, url)
-  if anchor.get_text() != '11':
-    print(anchor.get_text())
+  if anchor.get_text() != 'Last':
+    # print(anchor.get_text())
     await get_items(session, anchor.get('href'))
 
 async def init():
   init_sql()
   conn = aiohttp.TCPConnector(limit=25, ttl_dns_cache=3600)
   async with aiohttp.ClientSession(base_url=baseUrl, connector=conn) as session:
-    print('1')
+    # print('1')
     await get_items(session, begin)
   close_sql()
