99f93de64926305aeffb0fe32184e152c440a17b
atompkins <atompkins@gmail.com>
2021-11-03 20:00:22 +0000

    i don't trust it

--- src/init.py
+++ src/init.py
@@ -8,13 +8,13 @@ begin = 'index.php?cmd=items&index=0'
 async def get_items(session, url):
   anchor = await process_index_page(session, url)
   if anchor.get_text() != 'Last':
-    # print(anchor.get_text())
+    print(anchor.get_text())
     await get_items(session, anchor.get('href'))
 
 async def init():
   init_sql()
   conn = aiohttp.TCPConnector(limit=25, ttl_dns_cache=3600)
   async with aiohttp.ClientSession(base_url=baseUrl, connector=conn) as session:
-    # print('1')
+    print('1')
     await get_items(session, begin)
   close_sql()
